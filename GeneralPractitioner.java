d58671ca69b50076f2deef8d3384cb5d4a8a6732
monoLastui <2549844465@qq.com>
2024-11-14 16:45:36 +0800

    3.0

--- GeneralPractitioner.java
+++ GeneralPractitioner.java
@@ -1,15 +1,9 @@
 public class GeneralPractitioner extends HealthProfessional {
     private final String clinicName;  // 全科医生的诊所名称
 
-    // 默认构造函数
-    public GeneralPractitioner() {
-        super(0, "Unknown", "General Practitioner");
-        this.clinicName = "Unknown Clinic";
-    }
-
     // 带参数构造函数
     public GeneralPractitioner(int id, String name, String clinicName) {
-        super(id, name, "General Practitioner");
+        super(id, name, "General Practitioner"); // 假设 HealthProfessional 有这个构造函数
         this.clinicName = clinicName;
     }
 
@@ -22,7 +16,33 @@ public class GeneralPractitioner extends HealthProfessional {
     @Override
     public void displayInfo() {
         System.out.println("Health Professional Type: General Practitioner");
-        super.displayInfo();
+        super.displayInfo(); // 打印基本信息，如ID和姓名
         System.out.println("Clinic Name: " + clinicName);
+    }
+
+    // 添加 toString 方法
+    @Override
+    public String toString() {
+        return "GeneralPractitioner{" +
+                "id=" + getId() +
+                ", name='" + getName() + '\'' +
+                ", clinicName='" + clinicName + '\'' +
+                '}';
+    }
+
+    // 添加 equals 和 hashCode 方法
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GeneralPractitioner that = (GeneralPractitioner) o;
+        return getId() == that.getId() &&
+               Objects.equals(getName(), that.getName()) &&
+               Objects.equals(clinicName, that.clinicName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), clinicName);
     }
 }
