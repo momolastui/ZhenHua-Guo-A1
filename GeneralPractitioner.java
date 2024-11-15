03c5f7c29269052593b44ae61e78d168fb988a18
monoLastui <2549844465@qq.com>
2024-11-15 16:49:49 +0800

    29

--- GeneralPractitioner.java
+++ GeneralPractitioner.java
@@ -1,9 +1,15 @@
 public class GeneralPractitioner extends HealthProfessional {
     private final String clinicName;  // 全科医生的诊所名称
 
+    // 默认构造函数
+    public GeneralPractitioner() {
+        super(0, "Unknown", "General Practitioner");
+        this.clinicName = "Unknown Clinic";
+    }
+
     // 带参数构造函数
     public GeneralPractitioner(int id, String name, String clinicName) {
-        super(id, name, "General Practitioner"); // 假设 HealthProfessional 有这个构造函数
+        super(id, name, "General Practitioner");
         this.clinicName = clinicName;
     }
 
@@ -16,33 +22,7 @@ public class GeneralPractitioner extends HealthProfessional {
     @Override
     public void displayInfo() {
         System.out.println("Health Professional Type: General Practitioner");
-        super.displayInfo(); // 打印基本信息，如ID和姓名
+        super.displayInfo();
         System.out.println("Clinic Name: " + clinicName);
-    }
-
-    // 添加 toString 方法
-    @Override
-    public String toString() {
-        return "GeneralPractitioner{" +
-                "id=" + getId() +
-                ", name='" + getName() + '\'' +
-                ", clinicName='" + clinicName + '\'' +
-                '}';
-    }
-
-    // 添加 equals 和 hashCode 方法
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        GeneralPractitioner that = (GeneralPractitioner) o;
-        return getId() == that.getId() &&
-               Objects.equals(getName(), that.getName()) &&
-               Objects.equals(clinicName, that.clinicName);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getName(), clinicName);
     }
 }
--- Specialist.java
+++ Specialist.java
@@ -1,12 +1,6 @@
 public class Specialist extends HealthProfessional {
     private final String specialtyArea;  // 专科医生的专攻领域
 
-    // 默认构造函数
-    public Specialist() {
-        super(0, "Unknown", "Specialist");
-        this.specialtyArea = "Unknown Specialty";
-    }
-
     // 带参数构造函数
     public Specialist(int id, String name, String specialtyArea) {
         super(id, name, "Specialist");
@@ -22,7 +16,33 @@ public class Specialist extends HealthProfessional {
     @Override
     public void displayInfo() {
         System.out.println("Health Professional Type: Specialist");
-        super.displayInfo();
+        super.displayInfo(); // 假设这会打印ID和姓名
         System.out.println("Specialty Area: " + specialtyArea);
+    }
+
+    // 添加 toString 方法
+    @Override
+    public String toString() {
+        return "Specialist{" +
+                "id=" + getId() +
+                ", name='" + getName() + '\'' +
+                ", specialtyArea='" + specialtyArea + '\'' +
+                '}';
+    }
+
+    // 添加 equals 和 hashCode 方法
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Specialist specialist = (Specialist) o;
+        return getId() == specialist.getId() &&
+               Objects.equals(getName(), specialist.getName()) &&
+               Objects.equals(specialtyArea, specialist.specialtyArea);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), specialtyArea);
     }
 }
