7086a33edbdf9302affe1f6443e760dbaf756c1f
monoLastui <2549844465@qq.com>
2024-11-17 14:58:16 +0800

    2

--- HealthProfessional.java
+++ HealthProfessional.java
@@ -1,9 +1,16 @@
-public public class HealthProfessional {
+public class HealthProfessional {
     private int id;            // 医生编码
     private String name;       // 医生姓名
     private String specialty;  // 医生专业领域
 
-    // 构造函数：初始化姓名和专业
+    // 默认构造函数
+    public HealthProfessional() {
+        this.id = 0;
+        this.name = "Unknown";
+        this.specialty = "General";
+    }
+
+    // 带参构造函数，初始化所有实例变量
     public HealthProfessional(int id, String name, String specialty) {
         this.id = id;
         this.name = name;
@@ -15,19 +22,9 @@ public public class HealthProfessional {
         return id;
     }
 
-    // 设置医生编码
-    public void setId(int id) {
-        this.id = id;
-    }
-
     // 获取医生姓名
     public String getName() {
         return name;
-    }
-
-    // 设置医生姓名
-    public void setName(String name) {
-        this.name = name;
     }
 
     // 获取医生专业
@@ -35,16 +32,8 @@ public public class HealthProfessional {
         return specialty;
     }
 
-    // 设置医生专业
-    public void setSpecialty(String specialty) {
-        this.specialty = specialty;
-    }
-
-    // 打印医生信息
+    // 打印医生的信息
     public void displayInfo() {
         System.out.println("ID: " + id + ", Name: " + name + ", Specialty: " + specialty);
     }
-}
-  {
-    
 }
